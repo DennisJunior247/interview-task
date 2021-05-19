f87725f2ebc7346cdb17569a438e49c5dca0ebd6
dennisjunior247 <dennisjunior247@gmail.com>
2021-05-19 10:39:32 +0100

    started working on interview project

---
+++ src/screens/App..js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import AllTemplate from "../components/AllTemplates";
+
+function homeScreen() {
+  return (
+    <div>
+      <Header />
+      <AllTemplate />
+    </div>
+  );
+}
+
+export default homeScreen;
